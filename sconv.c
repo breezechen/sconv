5218af3353330ef8a7b1a137eba6380ca215e5f3
breeze <breezechen356@foxmail.com>
2016-11-17 00:15:07 +0800

    增加utf8与unicode互转

--- sconv.c
+++ sconv.c
@@ -113,3 +113,228 @@ int sconv_unicode_to_gbk(const WCHAR *wstr, int wlen, char *outbuf, int osize)
 	} while (0);
 	return ret;
 }
+
+
+//////////////////////////////////////////////////////////////////////////
+// Codes from iconv
+//////////////////////////////////////////////////////////////////////////
+
+/* Return code if invalid input after a shift sequence of n bytes was read.
+(xxx_mbtowc) */
+#define RET_SHIFT_ILSEQ(n)  (-1-2*(n))
+/* Return code if invalid. (xxx_mbtowc) */
+#define RET_ILSEQ           RET_SHIFT_ILSEQ(0)
+/* Return code if only a shift sequence of n bytes was read. (xxx_mbtowc) */
+#define RET_TOOFEW(n)       (-2-2*(n))
+
+/* Return code if invalid. (xxx_wctomb) */
+#define RET_ILUNI      -1
+/* Return code if output buffer is too small. (xxx_wctomb, xxx_reset) */
+#define RET_TOOSMALL   -2
+
+typedef unsigned int ucs4_t;
+
+
+/*
+* UTF-8
+*/
+
+/* Specification: RFC 3629 */
+
+static int
+utf8_mbtowc(ucs4_t *pwc, const unsigned char *s, int n)
+{
+	unsigned char c = s[0];
+
+	if (c < 0x80) {
+		*pwc = c;
+		return 1;
+	}
+	else if (c < 0xc2) {
+		return RET_ILSEQ;
+	}
+	else if (c < 0xe0) {
+		if (n < 2)
+			return RET_TOOFEW(0);
+		if (!((s[1] ^ 0x80) < 0x40))
+			return RET_ILSEQ;
+		*pwc = ((ucs4_t)(c & 0x1f) << 6)
+			| (ucs4_t)(s[1] ^ 0x80);
+		return 2;
+	}
+	else if (c < 0xf0) {
+		if (n < 3)
+			return RET_TOOFEW(0);
+		if (!((s[1] ^ 0x80) < 0x40 && (s[2] ^ 0x80) < 0x40
+			&& (c >= 0xe1 || s[1] >= 0xa0)))
+			return RET_ILSEQ;
+		*pwc = ((ucs4_t)(c & 0x0f) << 12)
+			| ((ucs4_t)(s[1] ^ 0x80) << 6)
+			| (ucs4_t)(s[2] ^ 0x80);
+		return 3;
+	}
+	else if (c < 0xf8 && sizeof(ucs4_t) * 8 >= 32) {
+		if (n < 4)
+			return RET_TOOFEW(0);
+		if (!((s[1] ^ 0x80) < 0x40 && (s[2] ^ 0x80) < 0x40
+			&& (s[3] ^ 0x80) < 0x40
+			&& (c >= 0xf1 || s[1] >= 0x90)))
+			return RET_ILSEQ;
+		*pwc = ((ucs4_t)(c & 0x07) << 18)
+			| ((ucs4_t)(s[1] ^ 0x80) << 12)
+			| ((ucs4_t)(s[2] ^ 0x80) << 6)
+			| (ucs4_t)(s[3] ^ 0x80);
+		return 4;
+	}
+	else if (c < 0xfc && sizeof(ucs4_t) * 8 >= 32) {
+		if (n < 5)
+			return RET_TOOFEW(0);
+		if (!((s[1] ^ 0x80) < 0x40 && (s[2] ^ 0x80) < 0x40
+			&& (s[3] ^ 0x80) < 0x40 && (s[4] ^ 0x80) < 0x40
+			&& (c >= 0xf9 || s[1] >= 0x88)))
+			return RET_ILSEQ;
+		*pwc = ((ucs4_t)(c & 0x03) << 24)
+			| ((ucs4_t)(s[1] ^ 0x80) << 18)
+			| ((ucs4_t)(s[2] ^ 0x80) << 12)
+			| ((ucs4_t)(s[3] ^ 0x80) << 6)
+			| (ucs4_t)(s[4] ^ 0x80);
+		return 5;
+	}
+	else if (c < 0xfe && sizeof(ucs4_t) * 8 >= 32) {
+		if (n < 6)
+			return RET_TOOFEW(0);
+		if (!((s[1] ^ 0x80) < 0x40 && (s[2] ^ 0x80) < 0x40
+			&& (s[3] ^ 0x80) < 0x40 && (s[4] ^ 0x80) < 0x40
+			&& (s[5] ^ 0x80) < 0x40
+			&& (c >= 0xfd || s[1] >= 0x84)))
+			return RET_ILSEQ;
+		*pwc = ((ucs4_t)(c & 0x01) << 30)
+			| ((ucs4_t)(s[1] ^ 0x80) << 24)
+			| ((ucs4_t)(s[2] ^ 0x80) << 18)
+			| ((ucs4_t)(s[3] ^ 0x80) << 12)
+			| ((ucs4_t)(s[4] ^ 0x80) << 6)
+			| (ucs4_t)(s[5] ^ 0x80);
+		return 6;
+	}
+	else
+		return RET_ILSEQ;
+}
+
+static int
+utf8_wctomb(unsigned char *r, ucs4_t wc, int n) /* n == 0 is acceptable */
+{
+	int count;
+	if (wc < 0x80)
+		count = 1;
+	else if (wc < 0x800)
+		count = 2;
+	else if (wc < 0x10000)
+		count = 3;
+	else if (wc < 0x200000)
+		count = 4;
+	else if (wc < 0x4000000)
+		count = 5;
+	else if (wc <= 0x7fffffff)
+		count = 6;
+	else
+		return RET_ILUNI;
+	if (n < count)
+		return RET_TOOSMALL;
+	switch (count) { /* note: code falls through cases! */
+	case 6: r[5] = 0x80 | (wc & 0x3f); wc = wc >> 6; wc |= 0x4000000;
+	case 5: r[4] = 0x80 | (wc & 0x3f); wc = wc >> 6; wc |= 0x200000;
+	case 4: r[3] = 0x80 | (wc & 0x3f); wc = wc >> 6; wc |= 0x10000;
+	case 3: r[2] = 0x80 | (wc & 0x3f); wc = wc >> 6; wc |= 0x800;
+	case 2: r[1] = 0x80 | (wc & 0x3f); wc = wc >> 6; wc |= 0xc0;
+	case 1: r[0] = wc;
+	}
+	return count;
+}
+// iconv end.
+
+
+int sconv_utf8_to_unicode(const char* utf8str, int slen, WCHAR* outbuf, int osize)
+{
+	int ret = -1;
+	const char* p = utf8str;
+	int i = 0, cnt = 0, cb;
+	WCHAR* op = outbuf;
+	ucs4_t wc;
+
+	do 
+	{
+		if (0 == utf8str) {
+			break;
+		}
+		if (slen < 0) {
+			slen = 0;
+			while (*p++) { ++slen; }
+			p = utf8str;
+		}
+		for (i = 0; i < slen;)
+		{
+			cb = utf8_mbtowc(&wc, p, slen - i);
+			if (cb <= 0) {
+				break;
+			}
+			i += cb;
+			p += cb;
+
+			if (op) {
+				if (cnt + 2 <= osize) {
+					*op++ = (WCHAR)wc;
+				} else {
+					break;
+				}
+			}
+
+			cnt += 2;
+		}
+		ret = cnt;
+	} while (0);
+
+	return ret;
+}
+
+
+int sconv_unicode_to_utf8(const WCHAR* wstr, int wlen, char* outbuf, int osize)
+{
+	int ret = -1;
+	const WCHAR* p = wstr;
+	int cnt = 0, i = 0, cb;
+	char* op = outbuf;
+	ucs4_t wc;
+	char tmp[4];
+
+	do
+	{
+		if (wstr == 0) {
+			break;
+		}
+		if (wlen < 0) {
+			wlen = 0;
+			while (*p++) { ++wlen; }
+			p = wstr;
+		}
+		for (i = 0; i < wlen * 2; i += 2, ++p)
+		{
+			wc = *p;
+
+			if (op) {
+				cb = utf8_wctomb(op, wc, osize - cnt);
+				if (cb <= 0) {
+					break;
+				}
+				op += cb;
+			}
+			else {
+				cb = utf8_wctomb(tmp, wc, 4);
+			}
+			
+			cnt += cb;
+		}
+		ret = cnt;
+	} while (0);
+
+	return ret;
+}
--- sconv.h
+++ sconv.h
@@ -14,8 +14,8 @@ extern "C" {
 int sconv_gbk_to_unicode(const char* gbkstr, int slen, WCHAR* outbuf, int osize);
 int sconv_unicode_to_gbk(const WCHAR* wstr, int wlen, char* outbuf, int osize);
 
-//int sconv_utf8_to_unicode(const char* utf8str, int slen, WCHAR* outbuf, int osize);
-//int sconv_unicode_to_utf8(const WCHAR* wstr, int wlen, char* outbuf, int osize);
+int sconv_utf8_to_unicode(const char* utf8str, int slen, WCHAR* outbuf, int osize);
+int sconv_unicode_to_utf8(const WCHAR* wstr, int wlen, char* outbuf, int osize);
 
 #ifdef __cplusplus
 }
